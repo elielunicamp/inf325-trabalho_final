1df6962bd22eaf88e067d6fc6a907439902f745a
Sandro Santana <inbox.sandro@gmail.com>
2024-09-21 10:06:33 -0300

    Resolving conflicts of the merge

--- docker/mariadb/init-epinga-db.sql
+++ docker/mariadb/init-epinga-db.sql
@@ -7,90 +7,55 @@ USE epinga_db;
 
 CREATE TABLE IF NOT EXISTS user (
     document INT PRIMARY KEY,
-    first_name VARCHAR(200) NOT NULL,
-    last_name VARCHAR(200) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100),
     password_hash VARCHAR(200) NOT NULL,
     register_date DATE
 );
 
+CREATE TABLE IF NOT EXISTS payment (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    payment_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS order_status (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    status_name VARCHAR(50)
+);
+
 CREATE TABLE IF NOT EXISTS product (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    merchant_document VARCHAR(20),
-    id_category BIGINT,
+    category VARCHAR(100),
     description VARCHAR(500),
     stock INT,
     price FLOAT
 );
 
-CREATE TABLE IF NOT EXISTS user_address (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    street VARCHAR(200),
-    city VARCHAR(100),
-    state VARCHAR(45),
-    zip_code VARCHAR(10),
-    country VARCHAR(45),
-    id_user_document BIGINT,
-    FOREIGN KEY (id_user_document) REFERENCES user(document)
-);
-
-CREATE TABLE IF NOT EXISTS order (
+CREATE TABLE IF NOT EXISTS orders (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id_payment BIGINT,
     user_document INT,
-    id_cart BIGINT,
-    id_user_address BIGINT,
     order_date DATETIME,
     payment_date DATETIME,
-    order_status VARCHAR(45),
-    order_total FLOAT,
+    destination_address VARCHAR(200),
+    id_order_status BIGINT,
     FOREIGN KEY (user_document) REFERENCES user(document),
-    FOREIGN KEY (id_user_address) REFERENCES address(id)
+    FOREIGN KEY (id_payment) REFERENCES payment(id),
+    FOREIGN KEY (id_order_status) REFERENCES order_status(id)
 );
 
-CREATE TABLE IF NOT EXISTS payment (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_document INT,
-    id_order BIGINT,
-    payment_date DATETIME,
-    payment_status VARCHAR(45),
-    FOREIGN KEY (user_document) REFERENCES user(document),
-    FOREIGN KEY (id_order) REFERENCES order(id)
-);
-
-CREATE TABLE IF NOT EXISTS order_itens (
+CREATE TABLE IF NOT EXISTS orders_itens (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_order BIGINT,
     id_product BIGINT,
-    FOREIGN KEY (id_order) REFERENCES order(id)
+    quantity INT,
+    unit_price FLOAT,
+    order_total FLOAT,
+    discount FLOAT,
+    FOREIGN KEY (id_order) REFERENCES order(id),
     FOREIGN KEY (id_product) REFERENCES product(id)
 );
 
-CREATE TABLE IF NOT EXISTS category_product (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    description VARCHAR(500)
-);
-
-CREATE TABLE IF NOT EXISTS merchant (
-    document VARCHAR(20) PRIMARY KEY,
-    phantasy_name VARCHAR(100),
-    phone VARCHAR(45)
-);
-
-CREATE TABLE IF NOT EXISTS cart (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_document INT,
-    dtAbertura DATETIME,
-    page VARCHAR(45),
-    FOREIGN KEY (user_document) REFERENCES user(document)
-);
-
-CREATE TABLE IF NOT EXISTS cart_itens (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    id_cart INT,
-    id_product INT,
-    FOREIGN KEY (id_cart) REFERENCES cart(id),
-    FOREIGN KEY (id_product) REFERENCES product(id)
-);
-
+commit;
